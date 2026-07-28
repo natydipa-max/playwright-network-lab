36dad1468829c94ab94f96511c3235ad507debdd
Natalia Di Paolo <natydipa@gmail.com>
2026-07-28 17:14:36 -0300

    feat(frontend): initialize React application with Vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Playwright Network Lab</h1>
+
+      <p>
+        Learn and experiment with Playwright Network Interception.
+      </p>
+
+      <button>
+        Load Users
+      </button>
+    </main>
+  )
+}
+
+export default App
