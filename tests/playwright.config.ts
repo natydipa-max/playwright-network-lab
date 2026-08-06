d971dae8d893e19ad5cde630e654321a96870f1b
Natalia Di Paolo <natydipa@gmail.com>
2026-08-06 12:12:39 -0300

    ci: add GitHub Actions workflow

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -1,8 +1,24 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test'
 
 export default defineConfig({
   testDir: './',
+
   use: {
     baseURL: 'http://localhost:5173',
   },
-});
+
+  webServer: [
+    {
+      command: 'npm run dev',
+      cwd: '../backend',
+      url: 'http://localhost:3000',
+      reuseExistingServer: true,
+    },
+    {
+      command: 'npm run dev',
+      cwd: '../frontend',
+      url: 'http://localhost:5173',
+      reuseExistingServer: true,
+    },
+  ],
+})
